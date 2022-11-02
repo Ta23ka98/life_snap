d83e2c16e619637f887404d52c419e461aa99bdb
taisei-kuro <t_kurohata@icloud.com>
2022-11-02 15:39:19 +0900

    Firebaseパッケージを追加し、ビルドが出来るか確認しました

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'presentation/firstPage/firstPage.dart';
 
-void main() {
+import 'firebase_options.dart';
+import 'presentation/firstPage/first_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
