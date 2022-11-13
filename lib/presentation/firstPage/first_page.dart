11b2cd9df2180a628edee4d390d7b787c23c8b8f
koki <oksg0722@gmail.com>
2022-11-13 13:22:52 +0900

    firebasecoreの追加

--- lib/presentation/firstPage/first_page.dart
+++ lib/presentation/firstPage/first_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
