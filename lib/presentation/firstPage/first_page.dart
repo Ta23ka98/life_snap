b0afa31a8304a1edf9cfee2650190d76831285da
koki <oksg0722@gmail.com>
2022-11-13 13:11:26 +0900

    FlutterFireCLIの追加

--- lib/presentation/firstPage/first_page.dart
+++ lib/presentation/firstPage/first_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
