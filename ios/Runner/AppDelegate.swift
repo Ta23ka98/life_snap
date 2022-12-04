1b137cba062e544136b8627db805ca4d4fd75e2e
まさたか <shinonome419@icloud.com>
2022-12-04 18:16:07 +0900

    update: AppDelegate.swiftをGoogleMapおよびそのAPIキーを間接的に呼び出すよう変更
    サンプルがswiftではなくobjective-cのものだったので、これでビルドが通るか不明

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Flutter
+import GoogleMaps
+import apikey.h
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +10,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey(maps_api_key)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
