2c37db39b04ea60b460afb3097439eb59fad40dc
まさたか <shinonome419@icloud.com>
2022-12-04 18:11:01 +0900

    update: main.dartにlib直下のファイルのapiKeyを呼び出すよう変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'apikey.dart';
 
 import 'firebase_options.dart';
 import 'presentation/first_page/first_page.dart';
+
+String APIKey = api_key;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
