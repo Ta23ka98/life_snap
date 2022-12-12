cee7384fddd7a22dfca90f047bdf69c6716b966b
koki <oksg0722@gmail.com>
2022-12-13 05:18:39 +0900

    画面外タップでtextformfieldを閉じれるように修正

--- lib/presentation/my_page/recertification_page.dart
+++ lib/presentation/my_page/recertification_page.dart
@@ -42,117 +42,130 @@ class _ReCertificationPageState extends State<ReCertificationPage> {
     final height = size.height;
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 89, 86, 86),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 100,
-            ),
-            const Profile(),
-            const SizedBox(height: 130),
-            Container(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: const [
-                        Text(
-                          'パスワード',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        fillColor: Colors.white,
-                        filled: true,
-                        contentPadding:
-                            const EdgeInsets.symmetric(vertical: 10),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(0)),
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque, //タッチ動作を検知する。
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 100,
+              ),
+              const Profile(),
+              const SizedBox(height: 130),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: const [
+                          Text(
+                            'パスワード',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
                       ),
-                      onChanged: (value) {
-                        password = value;
-                      },
-                    )
-                  ],
+                      TextFormField(
+                        decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          filled: true,
+                          contentPadding:
+                              const EdgeInsets.symmetric(vertical: 10),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(0)),
+                        ),
+                        onChanged: (value) {
+                          password = value;
+                        },
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 70),
-            SizedBox(
-              width: width - 30,
-              height: 70,
-              child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red,
-                      shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(0)))),
-                  onPressed: (() {
-                    showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            insetPadding:
-                                const EdgeInsets.only(left: 50, right: 50),
-                            titlePadding: const EdgeInsets.only(top: 100),
-                            content: SizedBox(
-                              width: width - 100,
-                              height: height / 5,
-                            ),
-                            buttonPadding: EdgeInsets.zero,
-                            shape: const RoundedRectangleBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(0.0))),
-                            title: const Center(
-                                child: Text(
-                              '本当に削除しますか?',
-                              style: TextStyle(color: Colors.red, fontSize: 15),
-                            )),
-                            actions: <Widget>[
-                              Row(
-                                children: [
-                                  SizedBox(
-                                    height: 70,
-                                    width: (width - 100) / 2,
-                                    child: ElevatedButton(
-                                        style: ElevatedButton.styleFrom(
-                                            backgroundColor: Colors.red,
-                                            shape: const RoundedRectangleBorder(
-                                                borderRadius: BorderRadius.all(
-                                                    Radius.circular(0)))),
-                                        onPressed: (() async {
-                                          deleteUser();
-                                        }),
-                                        child: const Text('削除する')),
-                                  ),
-                                  SizedBox(
-                                    height: 70,
-                                    width: (width - 100) / 2,
-                                    child: ElevatedButton(
-                                        style: ElevatedButton.styleFrom(
-                                            backgroundColor: Colors.white,
-                                            shape: const RoundedRectangleBorder(
-                                                borderRadius: BorderRadius.all(
-                                                    Radius.circular(0)))),
-                                        onPressed: (() {}),
-                                        child: const Text(
-                                          'しない',
-                                          style: TextStyle(color: Colors.blue),
-                                        )),
-                                  ),
-                                ],
-                              )
-                            ],
-                          );
-                        });
-                  }),
-                  child: const Text('アカウントを削除する')),
-            )
-          ],
+              const SizedBox(height: 70),
+              SizedBox(
+                width: width - 30,
+                height: 70,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                        shape: const RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(0)))),
+                    onPressed: (() {
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              insetPadding:
+                                  const EdgeInsets.only(left: 50, right: 50),
+                              titlePadding: const EdgeInsets.only(top: 100),
+                              content: SizedBox(
+                                width: width - 100,
+                                height: height / 5,
+                              ),
+                              buttonPadding: EdgeInsets.zero,
+                              shape: const RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(0.0))),
+                              title: const Center(
+                                  child: Text(
+                                '本当に削除しますか?',
+                                style:
+                                    TextStyle(color: Colors.red, fontSize: 15),
+                              )),
+                              actions: <Widget>[
+                                Row(
+                                  children: [
+                                    SizedBox(
+                                      height: 70,
+                                      width: (width - 100) / 2,
+                                      child: ElevatedButton(
+                                          style: ElevatedButton.styleFrom(
+                                              backgroundColor: Colors.red,
+                                              shape:
+                                                  const RoundedRectangleBorder(
+                                                      borderRadius:
+                                                          BorderRadius.all(
+                                                              Radius.circular(
+                                                                  0)))),
+                                          onPressed: (() async {
+                                            deleteUser();
+                                          }),
+                                          child: const Text('削除する')),
+                                    ),
+                                    SizedBox(
+                                      height: 70,
+                                      width: (width - 100) / 2,
+                                      child: ElevatedButton(
+                                          style: ElevatedButton.styleFrom(
+                                              backgroundColor: Colors.white,
+                                              shape:
+                                                  const RoundedRectangleBorder(
+                                                      borderRadius:
+                                                          BorderRadius.all(
+                                                              Radius.circular(
+                                                                  0)))),
+                                          onPressed: (() {}),
+                                          child: const Text(
+                                            'しない',
+                                            style:
+                                                TextStyle(color: Colors.blue),
+                                          )),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            );
+                          });
+                    }),
+                    child: const Text('アカウントを削除する')),
+              )
+            ],
+          ),
         ),
       ),
     );
