eb11b08edf88ee72fef7a4819ed168c41d32d92d
ke158 <74230682+ke158@users.noreply.github.com>
2022-12-15 16:23:21 +0900

    like_stateの編集

--- lib/state/like_state/like_state.dart
+++ lib/state/like_state/like_state.dart
@@ -2,7 +2,9 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:life_snap/domain/entity/liked_user/liked_user.dart';
 import 'package:life_snap/domain/entity/post/post.dart';
+import 'package:life_snap/infrastructure/repository/app_user_repository.dart';
 import 'package:life_snap/infrastructure/repository/liked_user_repository.dart';
+import 'package:life_snap/infrastructure/repository/my_like_repository.dart';
 
 import '../../common/print_log.dart';
 
@@ -14,20 +16,29 @@ class LikeState with _$LikeState {
 }
 
 final likeStateNotifierProvider =
-    StateNotifierProvider.autoDispose<LikedUserStateNotifier, LikeState>((ref) {
+    StateNotifierProvider.autoDispose<LikeStateNotifier, LikeState>((ref) {
   printLog(value: 'likeStateNotifierProvider init');
   ref.onDispose(() => printLog(value: 'likeStateNotifierProvider dispose'));
-  return LikedUserStateNotifier(
+  return LikeStateNotifier(
     likedUserRepository: ref.read(likedUserRepositoryProvider),
+    appUserRepository: ref.read(appUserRepositoryProvider),
+    myLikeRepository: ref.read(myLikeRepositoryProvider),
   );
 });
 
-class LikedUserStateNotifier extends StateNotifier<LikeState> {
-  LikedUserStateNotifier({required LikedUserRepository likedUserRepository})
+class LikeStateNotifier extends StateNotifier<LikeState> {
+  LikeStateNotifier(
+      {required LikedUserRepository likedUserRepository,
+      required AppUserRepository appUserRepository,
+      required MyLikeRepository myLikeRepository})
       : _likedUserRepository = likedUserRepository,
+        _appUserRepository = appUserRepository,
+        _myLikeRepository = myLikeRepository,
         super(const LikeState());
 
   final LikedUserRepository _likedUserRepository;
+  final AppUserRepository _appUserRepository;
+  final MyLikeRepository _myLikeRepository;
 
   Future<void> isLikes({required String uid, required Post post}) async {
     final String id = post.id;
@@ -37,12 +48,13 @@ class LikedUserStateNotifier extends StateNotifier<LikeState> {
   }
 
   Future<void> setLike({required String uid, required Post post}) async {
+    final postUserRef = _appUserRepository.getDocumentRef(uid: uid);
     final String id = post.id;
-    final newLikedUser = LikedUser(id: uid, createdAt: DateTime.now());
+    final newLikedUser =
+        LikedUser(id: uid, postUserRef: postUserRef, createdAt: DateTime.now());
     await _likedUserRepository.insert(
-      likedUser: newLikedUser,
       id: id,
-      uid: uid,
+      likedUser: newLikedUser,
     );
     state = state.copyWith(isFavolit: true);
   }
@@ -58,10 +70,12 @@ class LikedUserStateNotifier extends StateNotifier<LikeState> {
   Future<void> deleteAll({
     required String id,
   }) async {
-    final List<String> uids = await _likedUserRepository.getIds(id: id);
-    if (uids == []) {
+    final List<LikedUser> likedUsers =
+        await _likedUserRepository.getLikedUser(id: id);
+    if (likedUsers == []) {
       return;
     }
-    await _likedUserRepository.deleteAll(id: id, uids: uids);
+    await _myLikeRepository.deleteAll(id: id, likedUsers: likedUsers);
+    await _likedUserRepository.deleteAll(id: id, likedUsers: likedUsers);
   }
 }
