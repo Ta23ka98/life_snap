4c6e441f831f9790cfbda09c62808bbac6024eba
ke158 <74230682+ke158@users.noreply.github.com>
2022-12-21 17:02:04 +0900

    uret_like_list_pageの編集

--- lib/presentation/user_like_page/user_like_list_page.dart
+++ lib/presentation/user_like_page/user_like_list_page.dart
@@ -1,102 +1,176 @@
 import 'package:flutter/material.dart';
-import 'package:life_snap/presentation/user_like_page/user_like_check_page.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:life_snap/presentation/user_like_page/vm/user_like_page_notifier.dart';
+import 'package:life_snap/state/like_state/like_state.dart';
 
-class UserLikeListPage extends StatefulWidget {
+class UserLikeListPage extends HookConsumerWidget {
   const UserLikeListPage({super.key});
 
   @override
-  State<UserLikeListPage> createState() => _UserLikeListPageState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final _vm = ref.watch(userLikePageNotifierProvider.notifier);
+    final _postList = ref.watch(userLikePageNotifierProvider).posts;
 
-class _UserLikeListPageState extends State<UserLikeListPage> {
-  @override
-  Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
-    final double height = size.height;
-    final double width = size.width;
-    const String imageURL =
-        "https://www.bing.com/th?id=OIP.NGeQUv4Bg5uJOljwF0b_ggHaFj&w=176&h=185&c=8&rs=1&qlt=90&o=6&dpr=1.5&pid=3.1&rm=2";
-    const String text =
-        "朝起きると、まずは目の前の川に飛び込む。その後はキャンプファイヤーをしながらTwitterを開こうとすると圏外なので、もう一度川に入って忘れることにする。\nその後は車で街までドライブし、戻ってきてから魚を釣って夕食にする。";
+    // late String _uid;
+    // final _user = ref.read(userProvider);
+
+    // if (_user != null) {
+    //   _uid = _user.uid;
+    // }
+
+    const String _uid = "user1";
+
+    useEffect(() {
+      Future(() async {
+        await _vm.getMyLikePost(uid: _uid);
+      });
+
+      return null;
+    }, [_postList]);
+
     return Scaffold(
         backgroundColor: Colors.black,
         appBar: AppBar(
           title: const Text("いいねした投稿"),
+          centerTitle: true,
           backgroundColor: Colors.black,
         ),
-        body: StreamBuilder(
-          builder: (context, snapshot) {
-            return ListView(shrinkWrap: true, children: <Widget>[
-              for (int i = 0; i < 5; i++)
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: ((context) {
-                          return const UserLikeCheckPage(
-                              title: "モーニングルーティン",
-                              imageURL: imageURL,
-                              favCount: 74,
-                              text: text);
-                        }),
-                      ),
-                    );
-                  },
-                  child: Container(
-                    height: height * 0.45,
-                    width: width,
-                    decoration: const BoxDecoration(
-                      color: Color.fromARGB(255, 36, 36, 36),
-                    ),
-                    child: Column(
-                      children: <Widget>[
-                        SizedBox(
-                          width: width,
-                          child: const Text(
-                            "モーニング・ルーティン",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                        Container(
-                          height: height * 0.3,
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                                image: NetworkImage(imageURL),
-                                fit: BoxFit.fitWidth),
-                          ),
-                        ),
-                        Row(
-                          children: [
-                            IconButton(
-                              onPressed: () {},
-                              icon: const Icon(
-                                Icons.favorite,
-                                color: Colors.pink,
-                              ),
-                            ),
-                            const Text(
-                              "74",
-                              style: TextStyle(color: Colors.white),
-                            )
-                          ],
-                        ),
-                        SizedBox(
-                          height: height * 0.069,
-                          width: width * 0.8,
-                          child: const Text(
-                            text,
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-            ]);
-          },
-        ));
+        body: SafeArea(
+            child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: _postList != []
+              ? ListView.builder(
+                  padding: EdgeInsets.zero,
+                  scrollDirection: Axis.vertical,
+                  itemCount: _postList.length,
+                  itemBuilder: ((context, index) {
+                    final _post = _postList[index];
+                    final _likeVm =
+                        ref.watch(likeStateNotifierProvider.notifier);
+                    final _likeState = ref.watch(likeStateNotifierProvider);
+                    return _post != null
+                        ? FutureBuilder(
+                            future: _likeVm.isLikes(uid: _uid, post: _post),
+                            builder:
+                                (BuildContext context, AsyncSnapshot snapshot) {
+                              return Column(
+                                children: [
+                                  Container(
+                                    decoration: const BoxDecoration(
+                                      color: Color.fromARGB(255, 36, 36, 36),
+                                    ),
+                                    child: Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 10),
+                                        child: Column(
+                                          mainAxisSize: MainAxisSize.max,
+                                          children: [
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      vertical: 5),
+                                              child: Text(
+                                                _post.title,
+                                                style: const TextStyle(
+                                                    color: Colors.white),
+                                              ),
+                                            ),
+                                            _post.postImageURL != null
+                                                ? GestureDetector(
+                                                    onTap: () {
+                                                      //ページ遷移
+                                                      //             Navigator.push(
+                                                      //               context,
+                                                      //               MaterialPageRoute(
+                                                      //                 builder: ((context) {
+                                                      //                   return const UserLikeCheckPage(
+                                                      //                       title: "モーニングルーティン",
+                                                      //                       imageURL: imageURL,
+                                                      //                       favCount: 74,
+                                                      //                       text: text);
+                                                      //                 }),
+                                                    },
+                                                    child: Image.network(
+                                                      _post.postImageURL!,
+                                                      width: double.infinity,
+                                                      height: 200,
+                                                      fit: BoxFit.cover,
+                                                    ),
+                                                  )
+                                                : Container(),
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      vertical: 5),
+                                              child: Row(
+                                                mainAxisSize: MainAxisSize.max,
+                                                children: [
+                                                  IconButton(
+                                                    onPressed: () async {
+                                                      if (_likeState
+                                                              .isFavolit ==
+                                                          false) {
+                                                        await _likeVm.setLike(
+                                                            uid: _uid,
+                                                            post: _post);
+                                                        await _vm.increment(
+                                                            uid: _uid,
+                                                            post: _post);
+                                                      } else {
+                                                        await _likeVm
+                                                            .deleteLike(
+                                                                uid: _uid,
+                                                                post: _post);
+                                                        await _vm.decrement(
+                                                            uid: _uid,
+                                                            post: _post);
+                                                      }
+                                                    },
+                                                    icon: Icon(
+                                                      Icons.favorite,
+                                                      color: _likeState
+                                                                  .isFavolit ==
+                                                              true
+                                                          ? Colors.pink
+                                                          : Colors.white,
+                                                    ),
+                                                  ),
+                                                  Text(
+                                                    "${_post.likeCount}",
+                                                    style: const TextStyle(
+                                                      color: Colors.white,
+                                                    ),
+                                                  )
+                                                ],
+                                              ),
+                                            ),
+                                            // Container(
+                                            //   width: double.infinity,
+                                            //   height: 100,
+                                            //   decoration:
+                                            //       const BoxDecoration(),
+                                            //   child: Text(
+                                            //     "${_state.post!.likeCount}",
+                                            //     style: const TextStyle(
+                                            //         color: Colors.white),
+                                            //   ),
+                                            // ),
+                                          ],
+                                        )),
+                                  ),
+                                  const Divider(
+                                    thickness: 1,
+                                    height: 0,
+                                    color: Colors.white,
+                                  ),
+                                ],
+                              );
+                            })
+                        : Container();
+                  }),
+                )
+              : Container(),
+        )));
   }
 }
